4fdcc1ac505f8e00fec8b0867a0fd68ea6a2efb1
Jean-François ORNECH <jean-franois.ornech@merleauponty.fr>
2022-10-19 11:09:24 +0200

    Correction Issue#4 - Doublon questions - Ajout champs obligatoires

--- quizz.php
+++ quizz.php
@@ -13,25 +13,47 @@ $json = file_get_contents('json/quizz4.json');
 // Decode le contenue du fichier JSON
 $json_data = json_decode($json,true);
 
+// -------------------------
+// Préparation des questions
+// -------------------------
+
+// récupère le nombre de questions dans le fichier Json
+$nbr_questions = count($json_data["quizz"]);
+
+
 $questionnaire = array();
 
 for ($i = 1; $i <= 10; $i++) {
-   $question = rand(10,100);
-   array_push($questionnaire, $question);
+  // nombre aléatoire de 0 au nombre d'entrée du tableau list_questions
+   $q = rand(0,$nbr_questions);
+   if (in_array($q, $questionnaire))
+   {
+    // Vérifie si cette valeur est déja présente dans le tableau
+    // si oui refait un tour de boucle supplémentaire sans ajouter l'id de la question au tableau $questionnaire
+    //echo $q. " est PRESENT dans le tableau";
+    $i--;
+   }
+   else
+   {
+    //echo $q. " NON présent dans le tableau";
+    array_push($questionnaire, $q);
+    }
   }
  //var_dump($questionnaire);
+
+// Génération du questionnaire
 echo "<form action='resultat.php' method='post'>";
  foreach ($questionnaire as $key => $value) {
    // Question...
    echo "<p>".$json_data['quizz'][$value]['qcm_question']."";
-   echo "<fieldset>";
-   echo "<input type=radio name=$value value=r1>";
+   echo "<fieldset >";
+   echo "<input type=radio name=$value value=r1 required>";
    echo "<label for=r1>".$json_data['quizz'][$value]['qcm_r1_proposition']."</label><br>";
-   echo "<input type=radio name=$value value=r2>";
+   echo "<input type=radio name=$value value=r2 required>";
    echo "<label for=r2>".$json_data['quizz'][$value]['qcm_r2_proposition']."</label><br>";
-   echo "<input type=radio name=$value value=r3>";
+   echo "<input type=radio name=$value value=r3 required>";
    echo "<label for=r3>".$json_data['quizz'][$value]['qcm_r3_proposition']."</label><br>";
-   echo "<input type=radio name=$value value=r4>";
+   echo "<input type=radio name=$value value=r4 required>";
    echo "<label for=r4>".$json_data['quizz'][$value]['qcm_r4_proposition']."</label><br>";
    echo "</fieldset></p>";
   }
